76323faf495f46d81f66f2c532aa351e57b5e348
comacrae <comacrae1995@gmail.com>
2025-02-18 14:49:20 -0600

    added api wrappers

--- app/app/api.py
+++ app/app/api.py
@@ -1,4 +1,5 @@
 import requests, json
+import pandas as pd
 class Requester:
   def __init__(self, base_url:str="https://hunt-il-backend-fd6fc52c0ba0.herokuapp.com/api/")-> list:
     self.base_url = base_url
@@ -15,7 +16,39 @@ class Requester:
     return self.get_and_parse(url)
       
   def get_site_name_info(self,site_id:str) ->dict:
-    url = self.base_url + f"/sites/{site_id}/names"
+    url = self.base_url + f"sites/{site_id}/names"
+    return self.get_and_parse(url)
+  
+  def get_huntable_species(self) -> list:
+    url = self.base_url + 'huntable-species/'
+    return self.get_and_parse(url)
+  
+  def get_huntable_species_by_site(self, site_id:str) -> dict:
+    url = self.base_url + f"huntable-species/site/{site_id}"
+    return self.get_and_parse(url)
+
+  def get_sites_by_huntable_species(self, species:str) -> dict:
+    url = self.base_url + f"huntable-species/species/{species}"
+    return self.get_and_parse(url)
+  
+  def get_geography(self) -> list:
+    url = self.base_url + f"geography/"
+    return self.get_and_parse(url)
+
+  def get_geography_by_site(self, site_id:str) -> dict:
+    url = self.base_url + f"geography/{site_id}"
+    return self.get_and_parse(url)
+
+  def get_documents(self) -> list:
+    url = self.base_url + f"documents/"
+    return self.get_and_parse(url)
+
+  def get_documents_by_site(self, site_id:str) -> dict:
+    url = self.base_url + f"documents/{site_id}"
+    return self.get_and_parse(url)
+
+  def get_harvest(self) -> list:
+    url = self.base_url + f"harvest/"
     return self.get_and_parse(url)
 
 
